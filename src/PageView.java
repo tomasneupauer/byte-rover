392695dbe3196b8ca1dae4f3d39300573e8213af
tomasneupauer <tomas.neup.123@gmail.com>
2024-11-08 13:56:55 +0100

    switched to wysiwyg generated view

--- src/PageView.java
+++ src/PageView.java
@@ -4,46 +4,40 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JEditorPane;
 import javax.swing.JTree;
-import java.awt.GridBagLayout;
 
 public class PageView extends JPanel{
     private JPanel controlPanel;
+    
     private JPanel circuitPanel;
     private JScrollPane circuitScrollPane;
+    
     private JEditorPane contentEditorPane;
     private JScrollPane contentScrollPane;
+
     private JTree structureTree;
+    private JScrollPane structureScrollPane;
 
     public PageView(){
-        setLayout(new GridBagLayout());
-    }
-
-    public JPanel newControlPanel(){
         controlPanel = new JPanel();
-        controlPanel.setLayout(new GridBagLayout());
-        add(controlPanel, new Constraints(0, 0));
-        return controlPanel;
+        circuitPanel = new JPanel();
+        circuitScrollPane = new JScrollPane(circuitPanel);
+        contentEditorPane = new JEditorPane();
+        contentScrollPane = new JScrollPane(contentEditorPane);
+        structureTree = new JTree();
+        structureScrollPane = new JScrollPane(structureTree);
+        
+        add(controlPanel);
+        add(circuitScrollPane);
+        add(contentScrollPane);
+        controlPanel.add(structureScrollPane);
     }
 
-    public JTree newStructureTree(){
-        structureTree = new JTree();
-        controlPanel.add(structureTree, new Constraints(0, 0));
+    public JTree getStructureTree(){
         return structureTree;
     }
 
-    public JPanel newCircuitPanel(){
-        circuitPanel = new JPanel();
-        circuitPanel.setLayout(null);
-        circuitScrollPane = new JScrollPane(circuitPanel);
-        add(circuitScrollPane, new Constraints(1, 0));
-        return circuitPanel;
-    }
-
-    public JEditorPane newContentEditorPane(){
-       contentEditorPane = new JEditorPane();
-       contentScrollPane = new JScrollPane(contentEditorPane);
-       add(contentScrollPane, new Constraints(2, 0));
-       return contentEditorPane;
+    public JEditorPane getContentEditorPane(){
+        return contentEditorPane;
     }
 }
 
