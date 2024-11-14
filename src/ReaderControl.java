41b4a8f31904fac84d8fd1c3943b33b210d48443
tomasneupauer <tomas.neup.123@gmail.com>
2024-11-14 14:56:26 +0100

    fixed tree selection and root collapsing

--- src/ReaderControl.java
+++ src/ReaderControl.java
@@ -1,24 +1,27 @@
-package org.berandev.byterover;
+    package org.berandev.byterover;
 
-import javax.swing.JEditorPane;
-import javax.swing.JTree;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.DefaultTreeModel;
-import javax.swing.tree.TreePath;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.HashMap;
-import java.util.Map;
+    import javax.swing.JEditorPane;
+    import javax.swing.JTree;
+    import javax.swing.tree.DefaultMutableTreeNode;
+    import javax.swing.tree.DefaultTreeModel;
+    import javax.swing.tree.TreePath;
+    import javax.swing.tree.ExpandVetoException;
+    import javax.swing.event.TreeWillExpandListener;
+    import javax.swing.event.TreeExpansionEvent;
+    import java.awt.event.MouseAdapter;
+    import java.awt.event.MouseEvent;
+    import java.util.HashMap;
+    import java.util.Map;
 
-public class ReaderControl {
-    private ProjectModel projectModel;
-    private ReaderView readerView;
-    private JTree structureTree;
-    private JEditorPane contentEditorPane;
+    public class ReaderControl {
+        private ProjectModel projectModel;
+        private ReaderView readerView;
+        private JTree structureTree;
+        private JEditorPane contentEditorPane;
 
-    public ReaderControl(){
-        readerView = new ReaderView();
-        structureTree = readerView.getStructureTree();
+        public ReaderControl(){
+            readerView = new ReaderView();
+            structureTree = readerView.getStructureTree();
         contentEditorPane = readerView.getContentEditorPane();
     }
 
@@ -26,6 +29,7 @@ public class ReaderControl {
         this.projectModel = projectModel;
         structureTree.setModel(buildStructureTree());
         structureTree.addMouseListener(new TreeSelectionMouseListener());
+        structureTree.addTreeWillExpandListener(new TreeRootCollapseListener());
         contentEditorPane.setEditable(false);
         updatePageSelection();
     }
@@ -38,14 +42,25 @@ public class ReaderControl {
         @Override
         public void mousePressed(MouseEvent event){
             if (event.getClickCount() == 2 && event.getButton() == MouseEvent.BUTTON1){
-                int eventRow = structureTree.getRowForLocation(event.getX(), event.getY());
-                TreePath eventPath = structureTree.getPathForRow(eventRow);
+                TreePath eventPath = structureTree.getClosestPathForLocation(event.getX(), event.getY());
                 DefaultMutableTreeNode eventNode = (DefaultMutableTreeNode) eventPath.getLastPathComponent();
                 String eventNodeName = (String) eventNode.getUserObject();
                 if (projectModel.hasPage(eventNodeName)){
                     projectModel.setSelectedPageName(eventNodeName);
                     updatePageSelection();
                 }
+            }
+        }
+    }
+
+    private class TreeRootCollapseListener implements TreeWillExpandListener {
+        public void treeWillExpand(TreeExpansionEvent event) {}
+
+        public void treeWillCollapse(TreeExpansionEvent event) throws ExpandVetoException {
+            DefaultMutableTreeNode eventNode = (DefaultMutableTreeNode) event.getPath().getLastPathComponent();
+            DefaultMutableTreeNode rootNode = (DefaultMutableTreeNode) structureTree.getModel().getRoot();
+            if (eventNode.equals(rootNode)){
+                throw new ExpandVetoException(event);
             }
         }
     }
