58ff926a3e98f75493d4ee5a5e4e3232434735b2
tomasneupauer <tomas.neup.123@gmail.com>
2024-11-14 21:57:42 +0100

    separated common jtree utils

--- src/ReaderControl.java
+++ src/ReaderControl.java
@@ -1,35 +1,29 @@
-    package org.berandev.byterover;
+package org.berandev.byterover;
 
-    import javax.swing.JEditorPane;
-    import javax.swing.JTree;
-    import javax.swing.tree.DefaultMutableTreeNode;
-    import javax.swing.tree.DefaultTreeModel;
-    import javax.swing.tree.TreePath;
-    import javax.swing.tree.ExpandVetoException;
-    import javax.swing.event.TreeWillExpandListener;
-    import javax.swing.event.TreeExpansionEvent;
-    import java.awt.event.MouseAdapter;
-    import java.awt.event.MouseEvent;
-    import java.util.HashMap;
-    import java.util.Map;
+import javax.swing.JEditorPane;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreePath;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-    public class ReaderControl {
-        private ProjectModel projectModel;
-        private ReaderView readerView;
-        private JTree structureTree;
-        private JEditorPane contentEditorPane;
+public class ReaderControl {
+    private ProjectModel projectModel;
+    private ReaderView readerView;
+    private JTree projectTree;
+    private JEditorPane contentEditorPane;
 
-        public ReaderControl(){
-            readerView = new ReaderView();
-            structureTree = readerView.getStructureTree();
+    public ReaderControl(){
+        readerView = new ReaderView();
+        projectTree = readerView.getStructureTree();
         contentEditorPane = readerView.getContentEditorPane();
     }
 
     public void initControl(ProjectModel projectModel){
         this.projectModel = projectModel;
-        structureTree.setModel(buildStructureTree());
-        structureTree.addMouseListener(new TreeSelectionMouseListener());
-        structureTree.addTreeWillExpandListener(new TreeRootCollapseListener());
+        projectTree.setModel(TreeHandler.buildProjectTree(projectModel));
+        projectTree.addMouseListener(new TreeSelectionMouseListener());
+        TreeHandler.disableRootCollapse(projectTree);
         contentEditorPane.setEditable(false);
         updatePageSelection();
     }
@@ -38,11 +32,16 @@
         return readerView;
     }
 
+    private void updatePageSelection(){
+        contentEditorPane.setContentType(projectModel.getSelectedContentType());
+        contentEditorPane.setText(projectModel.readSelectedContent());
+    }
+
     private class TreeSelectionMouseListener extends MouseAdapter {
         @Override
         public void mousePressed(MouseEvent event){
             if (event.getClickCount() == 2 && event.getButton() == MouseEvent.BUTTON1){
-                TreePath eventPath = structureTree.getClosestPathForLocation(event.getX(), event.getY());
+                TreePath eventPath = projectTree.getClosestPathForLocation(event.getX(), event.getY());
                 DefaultMutableTreeNode eventNode = (DefaultMutableTreeNode) eventPath.getLastPathComponent();
                 String eventNodeName = (String) eventNode.getUserObject();
                 if (projectModel.hasPage(eventNodeName)){
@@ -51,38 +50,6 @@
                 }
             }
         }
-    }
-
-    private class TreeRootCollapseListener implements TreeWillExpandListener {
-        public void treeWillExpand(TreeExpansionEvent event) {}
-
-        public void treeWillCollapse(TreeExpansionEvent event) throws ExpandVetoException {
-            DefaultMutableTreeNode eventNode = (DefaultMutableTreeNode) event.getPath().getLastPathComponent();
-            DefaultMutableTreeNode rootNode = (DefaultMutableTreeNode) structureTree.getModel().getRoot();
-            if (eventNode.equals(rootNode)){
-                throw new ExpandVetoException(event);
-            }
-        }
-    }
-
-    private void updatePageSelection(){
-        contentEditorPane.setContentType(projectModel.getSelectedContentType());
-        contentEditorPane.setText(projectModel.readSelectedContent());
-    }
-
-    private DefaultTreeModel buildStructureTree(){
-        Map<String, DefaultMutableTreeNode> treeNodes = new HashMap<>();
-        String[] treeNodeNames = projectModel.getTreeNodeNames();
-        for (String treeNodeName : treeNodeNames){
-            treeNodes.put(treeNodeName, new DefaultMutableTreeNode(treeNodeName));
-        }
-        for (String treeNodeName : treeNodeNames){
-            String treeNodeParentName = projectModel.getTreeNodeParentName(treeNodeName);
-            if (!treeNodeName.equals(projectModel.getTreeRootName())){
-                treeNodes.get(treeNodeParentName).add(treeNodes.get(treeNodeName));
-            }
-        }
-        return new DefaultTreeModel(treeNodes.get(projectModel.getTreeRootName()));
     }
 }
 
