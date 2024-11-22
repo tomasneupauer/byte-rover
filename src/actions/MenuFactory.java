091850abd22b13597fcf1b0083287d0f0076a5df
tomasneupauer <tomas.neup.123@gmail.com>
2024-11-22 21:10:27 +0100

    done menu items updater

--- src/actions/MenuFactory.java
+++ src/actions/MenuFactory.java
@@ -5,59 +5,69 @@ import javax.swing.JPopupMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import javax.swing.Action;
+import javax.swing.MenuElement;
 import javax.swing.event.MenuListener;
 import javax.swing.event.MenuEvent;
 import java.awt.Component;
 
 public class MenuFactory {
     public static JPopupMenu newPopupMenu(Action[] items){
-        JPopupMenu actionPopupMenu = new ActionPopupMenu(items);
+        JPopupMenu actionPopupMenu = new ActionPopupMenu();
         for (Action action : items){
             actionPopupMenu.add(new JMenuItem(action));
+            Boolean separated = (Boolean) action.getValue(ActionFactory.SEPARATED);
+            if (separated.booleanValue()){
+                actionPopupMenu.addSeparator();
+            }
         }
         return actionPopupMenu;
     }
 
     public static JMenu newMenu(Action[] items, Action menu){
-        JMenu actionMenu = new ActionMenu(items, menu);
+        JMenu actionMenu = new ActionMenu(menu);
         for (Action action : items){
             actionMenu.add(new JMenuItem(action));
         }
         return actionMenu;
     }
+
+    public static void updateMenuElements(MenuElement menu){
+        MenuElement[] subElements = menu.getSubElements();
+        for (int i=0; i<subElements.length; i++){
+            if (subElements[i] instanceof JPopupMenu){
+                updateMenuElements(subElements[i]);
+            }
+            else {
+                updateMenuItem((JMenuItem) subElements[i]);
+            }
+        }
+    }
+
+    private static void updateMenuItem(JMenuItem item){
+        Action action = item.getAction();
+        action.putValue(ActionFactory.INVOKE_UPDATE, null);
+        Boolean visible = (Boolean) action.getValue(ActionFactory.VISIBLE);
+        item.setVisible(visible.booleanValue());
+    }
 }
 
 class ActionPopupMenu extends JPopupMenu {
-    private Action[] itemActions;
-
-    public ActionPopupMenu(Action[] items){
-        super();
-        itemActions = items;
-    }
-
     @Override
     public void show(Component invoker, int x, int y){
-        for (Action action : itemActions){
-            action.putValue(ActionFactory.INVOKE_UPDATE, null);
-        }
+        MenuFactory.updateMenuElements(this);
         SwingUtilities.invokeLater(() -> {super.show(invoker, x, y);});
     }
 }
 
 class ActionMenu extends JMenu {
-    private Action[] itemActions;
-
-    public ActionMenu(Action[] items, Action menu){
+    public ActionMenu(Action menu){
         super(menu);
-        itemActions = items;
         addMenuListener(new UpdateListener());
     }
 
     private class UpdateListener implements MenuListener {
         public void menuSelected(MenuEvent event){
-            for (Action action : itemActions){
-                action.putValue(ActionFactory.INVOKE_UPDATE, null);
-            }
+            MenuFactory.updateMenuElements(ActionMenu.this);
         }
         public void menuDeselected(MenuEvent event){}
         public void menuCanceled(MenuEvent event){}
